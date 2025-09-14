55d4e0eb1b74ac15c15944f1f6dafff6d89af3fb
Nathan0769 <nathanrivollier69@gmail.com>
2025-09-14 19:13:27 +0200

    Fix prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,7 +6,19 @@ const globalForPrisma = global as unknown as {
 };
 
 const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
