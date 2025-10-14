b05170b4f76a4855b729c1455cc3287788a34f7e
Nathan0769 <nathanrivollier69@gmail.com>
2025-10-14 10:30:11 +0200

    Fix: update fetch cache strategy to use revalidation for jersey data

--- app/jerseys/[leagueId]/clubs/[clubId]/jerseys/[id]/page.tsx
+++ app/jerseys/[leagueId]/clubs/[clubId]/jerseys/[id]/page.tsx
@@ -26,7 +26,7 @@ export async function generateMetadata({
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/api/jerseys/${id}`,
-      { cache: "no-store" }
+      { next: { revalidate: 3600 } }
     );
 
     if (!res.ok) {
