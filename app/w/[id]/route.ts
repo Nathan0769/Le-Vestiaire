d3cea6d9f6c5fa9c508592cc72bc2f4575b6ec10
Nathan0769 <nathanrivollier69@gmail.com>
2025-10-29 15:11:18 +0100

    Refactor: remove unused pages and components

--- app/w/[id]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const match = url.pathname.match(/\/w\/([^/]+)/);
-  const id = match?.[1];
-  return NextResponse.redirect(new URL(`/wishlist/share/${id}`, url.origin));
-}
