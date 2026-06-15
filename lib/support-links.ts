0673ded93bae6235ce1a3070b2788e00f6dcc428
Nathan0769 <nathanrivollier69@gmail.com>
2026-06-15 15:30:28 +0200

    feat: ajout section soutien avec Buy Me a Coffee et Tipeee
    Nouvelle section support en bas de la homepage avec boutons BMAC et Tipeee.
    Icônes de don ajoutées dans les liens sociaux de la sidebar.

---
+++ lib/support-links.ts
@@ -0,0 +1,3 @@
+// Mettre à jour ces URLs avec les vrais comptes une fois créés
+export const BUY_ME_A_COFFEE_URL = "https://www.buymeacoffee.com/levestiairefoot";
+export const TIPEEE_URL = "https://fr.tipeee.com/le-vestiaire-foot/";
